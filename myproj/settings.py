9e751e3eca1a4779355baa132f09ea4e247eedfd
Ethan <105538416+YiCheng1214@users.noreply.github.com>
2025-03-18 15:16:48 +0800

    Update settings.py

--- myproj/settings.py
+++ myproj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-fw058fqcz=h6ii@$&npzk_zv(tlfq#s+se=u5=)^=0h@$@h9+d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = 'myproj.urls'
